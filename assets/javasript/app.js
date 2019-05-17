0eddf4b50fda182217e5b2068f0bca4602d1d5b8
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-17 11:51:29 -0600

    created question object/global variables

---
+++ assets/javasript/app.js
@@ -0,0 +1,67 @@
+var questions = [{
+            question: 'What year was california made a state?',
+            answers: [{
+                    answer: 'A. 1850',
+                    value: true
+                },
+                {
+                    answer: 'B. 1824',
+                    value: false
+                },
+                {
+                    answer: 'C. 1798',
+                    value: false
+                },
+                {
+                    answer: "D. 1905",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: 'What year was california made a state?',
+            answers: [{
+                    answer: 'A. 1850',
+                    value: true
+                },
+                {
+                    answer: 'B. 1824',
+                    value: false
+                },
+                {
+                    answer: 'C. 1798',
+                    value: false
+                },
+                {
+                    answer: "D. 1905",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: 'What year was california made a state?',
+            answers: [{
+                    answer: 'A. 1850',
+                    value: true
+                },
+                {
+                    answer: 'B. 1824',
+                    value: false
+                },
+                {
+                    answer: 'C. 1798',
+                    value: false
+                },
+                {
+                    answer: "D. 1905",
+                    value: false
+                }
+            ]
+        },
+        var game;
+        var counter = 0;
+        var clock;
+        var timer = 30;
+        var correct = 0;
+        var incorrect = 0;
+        var unanswered = 0;
