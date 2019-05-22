cfe7806fe10e389c7c37552268deb47a6fbf9ed1
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-22 12:10:48 -0600

    made questions html, created reset and start functions

--- assets/javasript/app.js
+++ assets/javasript/app.js
@@ -1,5 +1,5 @@
 var questions = [{
-            question: 'What year was california made a state?',
+            question: '1. What year was california made a state?',
             answers: [{
                     answer: 'A. 1850',
                     value: true
@@ -19,7 +19,7 @@ var questions = [{
             ]
         },
         {
-            question: 'What year was california made a state?',
+            question: '2. What year was california made a state?',
             answers: [{
                     answer: 'A. 1850',
                     value: true
@@ -39,7 +39,7 @@ var questions = [{
             ]
         },
         {
-            question: 'What year was california made a state?',
+            question: '3. What year was california made a state?',
             answers: [{
                     answer: 'A. 1850',
                     value: true
@@ -57,7 +57,7 @@ var questions = [{
                     value: false
                 }
             ]
-        },
+        }]
         var game;
         var counter = 0;
         var clock;
@@ -65,3 +65,31 @@ var questions = [{
         var correct = 0;
         var incorrect = 0;
         var unanswered = 0;
+
+        //need another for loop inside the loop that already exists to loop through answers.
+        $("#start").on("click", 
+        function loop (){
+            console.log("loop");
+        for(var i = 0; i < questions.length; i++){
+            for (var j = 0; j <answers.length; j++){
+            $("#question").text(questions[i].question);
+            console.log("stuff", questions[i].question);
+            if(timer !== 0){
+                
+                if(timer === 12){//onclick is true
+                        //display you are correct 3 sec correct++
+                    }
+                    else{
+                        //incorrect +correct ans 3 sec incorrect++
+                    }
+            }
+            else{
+                //you ran out of time + correct ans for 3 seconds unanswered++
+
+            }
+            
+    }
+}
+})
+
+// loop();
