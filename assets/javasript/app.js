0d3920c6c5de06a1a55188fbd1b59321e6bb029f
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-22 12:55:42 -0600

    stuff got very jumbled starting again with a new game plan

--- assets/javasript/app.js
+++ assets/javasript/app.js
@@ -1,95 +1,42 @@
-var questions = [{
-            question: '1. What year was california made a state?',
-            answers: [{
-                    answer: 'A. 1850',
-                    value: true
-                },
-                {
-                    answer: 'B. 1824',
-                    value: false
-                },
-                {
-                    answer: 'C. 1798',
-                    value: false
-                },
-                {
-                    answer: "D. 1905",
-                    value: false
-                }
-            ]
-        },
-        {
-            question: '2. What year was california made a state?',
-            answers: [{
-                    answer: 'A. 1850',
-                    value: true
-                },
-                {
-                    answer: 'B. 1824',
-                    value: false
-                },
-                {
-                    answer: 'C. 1798',
-                    value: false
-                },
-                {
-                    answer: "D. 1905",
-                    value: false
-                }
-            ]
-        },
-        {
-            question: '3. What year was california made a state?',
-            answers: [{
-                    answer: 'A. 1850',
-                    value: true
-                },
-                {
-                    answer: 'B. 1824',
-                    value: false
-                },
-                {
-                    answer: 'C. 1798',
-                    value: false
-                },
-                {
-                    answer: "D. 1905",
-                    value: false
-                }
-            ]
-        }]
-        var game;
-        var counter = 0;
-        var clock;
-        var timer = 30;
-        var correct = 0;
-        var incorrect = 0;
-        var unanswered = 0;
 
-        //need another for loop inside the loop that already exists to loop through answers.
-        $("#start").on("click", 
-        function loop (){
-            console.log("loop");
-        for(var i = 0; i < questions.length; i++){
-            for (var j = 0; j <answers.length; j++){
-            $("#question").text(questions[i].question);
-            console.log("stuff", questions[i].question);
-            if(timer !== 0){
-                
-                if(timer === 12){//onclick is true
-                        //display you are correct 3 sec correct++
-                    }
-                    else{
-                        //incorrect +correct ans 3 sec incorrect++
-                    }
-            }
-            else{
-                //you ran out of time + correct ans for 3 seconds unanswered++
-
-            }
-            
-    }
-}
-})
-
-// loop();
+var game;
+var counter = 0;
+var clock;
+var timer = 30;
+var questions = [
+    {
+      question: "What is the average length of a male Black & White Argentine Tegu?",
+      answers: [
+        { answer: "4 feet", value: false },
+        { answer: "4.5 feet", value: true },
+        { answer: "5 feet", value: false },
+        { answer: "5.5 feet", value: false }
+      ]
+    },
+    {
+      question: "Which country do panther chameleons originate from?",
+      answers: [
+        { answer: "China", value: false },
+        { answer: "Brazil", value: false },
+        { answer: "Australia", value: false },
+        { answer: "Madagascar", value: true }
+      ]
+    },
+    {
+      question: "How many chambers does a snake have in it's heart?",
+      answers: [
+        { answer: "3", value: true },
+        { answer: "2", value: false },
+        { answer: "4", value: false },
+        { answer: "1", value: false }
+      ]
+    },
+    {
+      question: "What is the average lifespan of a captive bred leopard gecko",
+      answers: [
+        { answer: "5 years", value: false },
+        { answer: "8 years", value: false },
+        { answer: "10 years", value: true },
+        { answer: "15 years", value: false }
+      ]
+    }]
